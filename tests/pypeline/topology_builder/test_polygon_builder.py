7fba84f040da069144286d4cccf75b99eabb979b
Ayasse, Carolin <carolin.ayasse@tu-darmstadt.de>
2026-05-04 11:04:12 +0200

    fixed PolygonTopologyBuilder test

--- tests/pypeline/topology_builder/test_polygon_builder.py
+++ tests/pypeline/topology_builder/test_polygon_builder.py
@@ -2,20 +2,7 @@
 """Tests for the polygon-based topology builder."""
 
 from pypeline.topology_builder import TopologyBuildResult
-from pypeline.topology_builder.polygon_builder import build_topology_from_polygons
-
 from pypeline.topology_builder.topology_builder import PolygonTopologyBuilder
-
-
-def test_polygon_builder(streets_data_path, polygons_data_path):
-    build_result = build_topology_from_polygons(streets_data_path, polygons_data_path, region_id_column='id',
-                                                default_region=None, streets_geometry_column_name="geometry",
-                                                polygons_geometry_column_name="geometry")
-
-    assert isinstance(build_result, TopologyBuildResult)
-    for region_name in range(9):
-        assert region_name in build_result.streets["id"]
-
 
 def test_polygon_topology_builder(streets_data_path, polygons_data_path):
     builder = PolygonTopologyBuilder()
