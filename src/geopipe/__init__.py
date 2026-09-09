04ea4c5ea249e5d40e9fb4bfcd9c00929f7f02e6
Ayasse, Carolin <carolin.ayasse@tu-darmstadt.de>
2026-09-09 16:48:31 +0200

    prepared for package publication

--- src/geopipe/__init__.py
+++ src/geopipe/__init__.py
@@ -1,7 +1,11 @@
+import importlib.metadata
+
 from geopipe.energy_system import (
     EnergySystem,
     EnergySystemBuilder,
     EnergySystemBuilderConfig,
 )
 from geopipe.data.data_registry import DataRegistry
+
+__version__ = importlib.metadata.version("geopipe")
 
